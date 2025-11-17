3228cda3a29b88f88ccfecc7719c10a0b11b72d1
Garri Figueroa <gfiguero@gmail.com>
2025-11-17 13:01:28 -0300

    Add user authentication and custom flash message types

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,10 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  before_action :authenticate_user!
+  add_flash_types :success, :danger, :warning, :info, :error, :created, :updated, :deleted
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
 end
