6cc1e9e88d3052834e04421d7e35c3e4f42d8193
Garri Figueroa <gfiguero@gmail.com>
2025-11-17 13:06:21 -0300

    Add first_name and last_name to users and improve scaffold show template with i18n support

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_11_16_004852) do
+ActiveRecord::Schema[8.1].define(version: 2025_11_16_143814) do
   create_table "categories", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "name"
@@ -38,6 +38,8 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_16_004852) do
     t.datetime "created_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.datetime "remember_created_at"
     t.datetime "reset_password_sent_at"
     t.string "reset_password_token"
