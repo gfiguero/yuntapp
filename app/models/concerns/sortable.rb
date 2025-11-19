f01cbd752b999bd2801777a95458f4d31274040b
Garri Figueroa <gfiguero@gmail.com>
2025-11-19 09:55:46 -0300

    Add `sort_by_active` scope to Sortable concern

--- app/models/concerns/sortable.rb
+++ app/models/concerns/sortable.rb
@@ -4,6 +4,7 @@ module Sortable
   included do
     scope :sort_by_id, ->(direction = nil) { direction.present? ? reorder(id: direction) : reorder(id: :asc) }
     scope :sort_by_name, ->(direction = nil) { direction.present? ? reorder(name: direction) : reorder(name: :asc) }
+    scope :sort_by_active, ->(direction = nil) { direction.present? ? reorder(active: direction) : reorder(active: :asc) }
     scope :sort_by_created_at, ->(direction = nil) { direction.present? ? reorder(created_at: direction) : reorder(created_at: :asc) }
   end
 end
