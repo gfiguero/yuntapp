e835a7d790ef89c807a187400dc45b2b87e9fa78
Garri Figueroa <gfiguero@gmail.com>
2025-12-30 10:45:53 -0300

    Add `admin` column to `users` table for role management

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_11_16_143814) do
+ActiveRecord::Schema[8.1].define(version: 2025_12_30_054301) do
   create_table "categories", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "name"
@@ -35,6 +35,7 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_16_143814) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.boolean "admin"
     t.datetime "created_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
