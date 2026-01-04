82d96591e8106cc81ee2eb398b42a3544ad6c44b
Garri Figueroa <gfiguero@gmail.com>
2026-01-04 01:27:24 -0300

    Enable SimpleCov for test coverage tracking and disable test parallelization

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,6 @@
+require "simplecov"
+SimpleCov.start "rails"
+
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
@@ -5,7 +8,7 @@ require "rails/test_help"
 module ActiveSupport
   class TestCase
     # Run tests in parallel with specified workers
-    parallelize(workers: :number_of_processors)
+    # parallelize(workers: :number_of_processors)
 
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all
