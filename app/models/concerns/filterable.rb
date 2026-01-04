87ad8eb80959809a99ae878d7e04a1776db47f7e
Garri Figueroa <gfiguero@gmail.com>
2026-01-04 01:01:41 -0300

    Update `filter_by_id` scope to support filtering by multiple IDs

--- app/models/concerns/filterable.rb
+++ app/models/concerns/filterable.rb
@@ -2,7 +2,7 @@ module Filterable
   extend ActiveSupport::Concern
 
   included do
-    scope :filter_by_id, ->(id) { where(id: id) }
+    scope :filter_by_id, ->(ids) { where(id: ids) }
     scope :filter_by_name, ->(name) { where.like(name: "%#{name}%") }
   end
 end
